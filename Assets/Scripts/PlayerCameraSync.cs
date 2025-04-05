1fef0e492b4c93d4344f8d6a2b1fc65b2c47a3f4
RangerHalt2 <141537190+RangerHalt2@users.noreply.github.com>
2025-04-05 00:50:30 -0400

    Spell Rotation Sync
    syncs the player's look direction with their camera direction when they're using the spells.

---
+++ Assets/Scripts/PlayerCameraSync.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCameraSync : MonoBehaviour
+{
+    [SerializeField] private Transform playerBob;
+    [SerializeField] private Transform orientation;
+
+    private InputHandler myInputs;
+
+    private void Start()
+    {
+        myInputs = GameObject.FindAnyObjectByType<InputHandler>();
+    }
+
+    private void Update()
+    {
+        if(myInputs.altFireTriggered || myInputs.fireTriggered)
+        {
+            playerBob.forward = orientation.forward;
+        }
+    }
+}
