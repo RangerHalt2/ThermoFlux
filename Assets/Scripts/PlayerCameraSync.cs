fb1eab9c118cd7018acede2fb05f7c84a6133836
JaimeA47 <149345697+JaimeA47@users.noreply.github.com>
2025-04-05 21:41:11 -0400

    TrailerDEMO
    Saturday 5 9:41pm

--- Assets/Scripts/PlayerCameraSync.cs
+++ Assets/Scripts/PlayerCameraSync.cs
@@ -1,3 +1,5 @@
+//Author: Logan Baysinger
+//Purpose: 
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,6 +7,7 @@ using UnityEngine;
 public class PlayerCameraSync : MonoBehaviour
 {
     [SerializeField] private Transform playerBob;
+    [SerializeField] private Transform obj;
     [SerializeField] private Transform orientation;
 
     private InputHandler myInputs;
@@ -19,6 +22,7 @@ public class PlayerCameraSync : MonoBehaviour
         if(myInputs.altFireTriggered || myInputs.fireTriggered)
         {
             playerBob.forward = orientation.forward;
+            obj.forward = orientation.forward;
         }
     }
 }
